c1087a880a3fed79788a33c7fa688b2af4e67d27
manasa130 <62628660+manasa130@users.noreply.github.com>
2023-07-12 15:54:04 -0500

    Add files via upload

---
+++ assignment1.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	
+)
+
+func arithmetics(x, y int) (result int) {
+	fmt.Println("Addition: ", x+y)
+	fmt.Println("Subtraction: ", x-y)
+	fmt.Println("Multiplication: ", x*y)
+	fmt.Println("Division: ", x/y)
+	fmt.Println("Percentage: ", x%y)
+
+	return
+}
+
+func logicalexp()
+
+func main() {
+	arithmetics(20, 4)
+}
